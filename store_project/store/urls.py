5e118160c0a6ecc80cbff9ab2f7e87755556dc01
Ronaldo <yaperdole26@gmail.com>
2024-12-05 01:41:26 -0600

    Added superuser

--- store_project/store/urls.py
+++ store_project/store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list, product_detail
 
 urlpatterns = [
     path('store/', product_list),
+    path('detail/', product_detail, name='detail')
 ]
--- store_project/store/views.py
+++ store_project/store/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from store.models import Product
 
+
+def build_template(lst: list, cols: int) -> list[list]:
+    return [lst[i: i + cols] for i in range(0, len(lst), cols)]
+    
 
 def product_list(request):
-    return HttpResponse('Hello world!')
+    products = Product.objects.all()
+    return render(
+        request,
+        'store/product_list.html', 
+        context = {
+            'product_list': build_template(products, 3),
+        }
+    )
+    
+
+def product_detail(request):
+    return render(request, 'store/product_detail.html')
